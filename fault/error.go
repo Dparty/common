ff769bca8c94b4969c6b94ebf4b0f30a9168f479
Dan Chen <dan.chen@boardware.com>
2023-09-29 11:37:26 +0800

    feat

---
+++ fault/error.go
@@ -0,0 +1,75 @@
+package fault
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var ErrBadRequest = errors.New("bad request")
+var ErrUnauthorized = errors.New("unauthorized")
+var ErrPermissionDenied = errors.New("permission denied")
+var ErrNotFound = errors.New("not found")
+var ErrTooManyRequests = errors.New("too many requests")
+var ErrEmailExists = errors.New("email exists")
+var ErrUndefined = errors.New("undefined error")
+var ErrVerificationCode = errors.New("verification code error")
+var ErrEnum = errors.New("enum parse error")
+
+type ErrorCode int
+
+const (
+	BAD_REQUEST_ERROR             ErrorCode = http.StatusBadRequest
+	UNATHORIZED_ERROR             ErrorCode = 401
+	PERMISSION_ERROR              ErrorCode = 403
+	NOT_FOUND_ERROR               ErrorCode = 404
+	TOO_MANY_REQUESTS             ErrorCode = 429
+	EMAIL_EXISTS_ERROR            ErrorCode = 6002
+	AUTHENTICATION_ERROR          ErrorCode = 6003
+	SERVICE_KEY_DUPLICATION_ERROR ErrorCode = 6004
+	VERIFICATION_CODE_ERROR       ErrorCode = 6005
+	VERIFICATION_CODE_FREQUENT    ErrorCode = 6006
+	UNDEFINE_ERROR                ErrorCode = 9999
+)
+
+func GetCode(err error) (statusCode int, errorCode ErrorCode) {
+	switch {
+	case errors.Is(err, ErrBadRequest):
+		statusCode = http.StatusBadRequest
+		errorCode = BAD_REQUEST_ERROR
+	case errors.Is(err, ErrUnauthorized):
+		statusCode = http.StatusUnauthorized
+		errorCode = UNATHORIZED_ERROR
+	case errors.Is(err, ErrPermissionDenied):
+		statusCode = http.StatusForbidden
+		errorCode = PERMISSION_ERROR
+	case errors.Is(err, ErrNotFound):
+		statusCode = http.StatusNotFound
+		errorCode = NOT_FOUND_ERROR
+	case errors.Is(err, ErrTooManyRequests):
+		statusCode = http.StatusTooManyRequests
+		errorCode = TOO_MANY_REQUESTS
+	case errors.Is(err, ErrEmailExists):
+		statusCode = http.StatusForbidden
+		errorCode = EMAIL_EXISTS_ERROR
+	case errors.Is(err, ErrVerificationCode):
+		statusCode = http.StatusPreconditionFailed
+		errorCode = VERIFICATION_CODE_ERROR
+	case errors.Is(err, ErrUndefined):
+		statusCode = http.StatusInternalServerError
+		errorCode = UNDEFINE_ERROR
+	default:
+		statusCode = http.StatusInternalServerError
+		errorCode = UNDEFINE_ERROR
+	}
+	return statusCode, errorCode
+}
+
+func GinHandler(ctx *gin.Context, err error) {
+	statusCode, errorCode := GetCode(err)
+	ctx.JSON(statusCode, gin.H{
+		"code":    errorCode,
+		"message": err.Error(),
+	})
+}

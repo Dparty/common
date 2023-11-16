4e6f998c5355e6e906f8d537118a16d40ca19fdd
Dan Chen <dan.chen@boardware.com>
2023-11-16 15:38:27 +0800

    feat

--- abstract/interfaces.go
+++ abstract/interfaces.go
@@ -1,11 +1,24 @@
 package interfaces
 
+import "reflect"
+
 type Owner interface {
 	ID() uint
 	Own(asset Asset) bool
+	Owner() Owner
 }
 
 type Asset interface {
 	SetOwner(owner Owner) Asset
 	Owner() Owner
+}
+
+func Own(owner Owner, asset Asset) bool {
+	if owner == nil {
+		return false
+	}
+	if owner.ID() == asset.Owner().ID() && reflect.TypeOf(owner) == reflect.TypeOf(asset.Owner()) {
+		return true
+	}
+	return Own(owner.Owner(), asset)
 }

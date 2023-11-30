808ccb156deb9dfed31b66c7d14ad9f6cb9be139
Dan Chen <dan.chen@boardware.com>
2023-11-30 17:33:06 +0800

    feat

--- server/server.go
+++ server/server.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)

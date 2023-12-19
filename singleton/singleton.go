ebdd752c83e67566a019fdb52c7635e790265736
Dan Chen <dan.chen@boardware.com>
2023-12-19 12:15:24 +0800

    feat

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -7,32 +7,44 @@ type Singleton[T any] interface {
 }
 
 type EagerSingleton[T any] struct {
-	entity *T
+	instance *T
 }
 
-func NewEagerSingleton[T any](constructor func() *T) EagerSingleton[T] {
+func (s *EagerSingleton[T]) Get() *T {
+	return s.instance
+}
+
+func NewSingleton[T any](constructor func() *T, eagerModel bool) Singleton[T] {
+	if eagerModel {
+		return NewEagerSingleton[T](constructor)
+	} else {
+		return NewLazySingleton[T](constructor)
+	}
+}
+
+func NewEagerSingleton[T any](constructor func() *T) *EagerSingleton[T] {
 	var singleton EagerSingleton[T]
-	singleton.entity = constructor()
-	return singleton
+	singleton.instance = constructor()
+	return &singleton
 }
 
 type LazySingleton[T any] struct {
-	entity      *T
+	instance    *T
 	constructor func() *T
 	lock        sync.Mutex
 }
 
-func NewLazySingleton[T any](constructor func() *T) LazySingleton[T] {
+func NewLazySingleton[T any](constructor func() *T) *LazySingleton[T] {
 	var singleton LazySingleton[T]
 	singleton.constructor = constructor
-	return singleton
+	return &singleton
 }
 
 func (s *LazySingleton[T]) Get() *T {
 	s.lock.Lock()
-	if s.entity == nil {
-		s.entity = s.constructor()
+	if s.instance == nil {
+		s.instance = s.constructor()
 	}
 	s.lock.Unlock()
-	return s.entity
+	return s.instance
 }
